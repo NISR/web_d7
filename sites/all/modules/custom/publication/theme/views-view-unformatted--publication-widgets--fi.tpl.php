9742bdb67ffbfce5dc53eb6e7e8959eb17a11118
Dominique Kariningufu <dominique@tayaricentral.com>
2015-12-24 00:30:06 +0200

    Features update

--- sites/all/modules/custom/publication/theme/views-view-unformatted--publication-widgets--fi.tpl.php
+++ sites/all/modules/custom/publication/theme/views-view-unformatted--publication-widgets--fi.tpl.php
@@ -7,15 +7,14 @@
  * @ingroup views_templates
  */
  $featured_box = array("featured-box-primary","featured-box-secondary","featured-box-tertiary","featured-box-quaternary");
- $index = 0;
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
 	<div class="col-md-3">
-		<div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .' featured-box '. $featured_box[$index++] .' "';  } ?> style="cursor:pointer">
-			<div class="box-content">
+		<div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .' featured-box '. $featured_box[$id] .' "';  } ?> style="cursor:pointer">
+			<div class="box-content" style="text-align:center">
 				<?php print $row; ?>
 			</div>
 		</div>
