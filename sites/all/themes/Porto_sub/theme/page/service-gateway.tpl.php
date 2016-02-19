a6963545aaa11be762d472d1ccc3b7f5508dbbf4
Dominique Kariningufu <dominique@tayaricentral.com>
2016-02-19 15:35:53 +0200

    authentication service update

--- sites/all/themes/Porto_sub/theme/page/service-gateway.tpl.php
+++ sites/all/themes/Porto_sub/theme/page/service-gateway.tpl.php
@@ -4,26 +4,21 @@
  * Service Authentication gateway page
  */
 ?>
-<div class="authentication-top-menu"><a href=""><h1><< <?php print t('Back to site');?></h1></a></div>    	
-<div class="body">
-	<div role="main" class="main">
-	  <div id="content" class="content full col-md-6">
-	    <div class="container ">
-	    	<div class="row">
-				
-	    	</div>
-	      <div class="row">
-			    <div class="col-md-6">
-		       	 <?php print $messages; ?>
-			    </div>
-			</div>
-			<div class="row">
-				 <div class=""></div>
-			    <div class="col-md-6 authentication-box">
-					  <?php if (isset($page['content'])) { print render($page['content']); } ?>	      
-				 </div>
-			</div>
-	    </div>  
-	  </div>  	  
-</div>  
+<div class="authentication-top-menu"><a href=""><h1><< <?php print t('Back to site');?></h1></a></div> 
+
+<div role="main" class="main">
+  <div id="content" class="content full col-md-12">
+    <div class="container ">
+		<div class="row">   	
+		    <div class="login-box">
+				  <div class="wrapper-box">
+				  		<?php print $messages; ?>
+				  		<?php if (isset($page['content'])) { print render($page['content']); } ?>
+				  	</div>   
+			 </div>
+		</div>
+    </div>  
+  </div>  	  
+</div>
+  
   
