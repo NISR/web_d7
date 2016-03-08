ea9f526da525ee48ce9b544e9079124dccafa0f4
Dominique Kariningufu <dominique@tayaricentral.com>
2016-03-08 17:07:51 +0200

    Added password recovery routine inspired from user module

--- sites/all/modules/custom/nisr_service/theme/nisr_service_gateway.tpl.php
+++ sites/all/modules/custom/nisr_service/theme/nisr_service_gateway.tpl.php
@@ -13,7 +13,7 @@
   <div id="content" class="content full col-md-12">
     <div class="container">
 		<div class="row authentication-box">
-				<div class="message"><?php print $messages; ?></div>
+				<div class="message centered"><?php print $messages; ?></div>
 				<div class="image-logo"><img src="<?php print drupal_get_path('module','nisr_service');?>/images/nisr_logo_vertical.png"></div>
 				<div><?php if (isset($page['content'])) { print render($page['content']); } ?></div>
 		</div>
---
+++ sites/all/modules/custom/nisr_service/theme/nisr_service_recover_password.tpl.php
@@ -0,0 +1,5 @@
+<div class="recover-password-box">
+	<div class="wrapper-box">
+		<?php print drupal_render_children($form); ?>
+	</div>
+</div>
