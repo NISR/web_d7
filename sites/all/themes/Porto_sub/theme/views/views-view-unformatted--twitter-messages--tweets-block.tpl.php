b47c2d3adef73c06da9cad4bfe812067623ed68e
Dominique Kariningufu <dkariningufu@gmail.com>
2017-02-03 13:06:57 +0200

    Ad Secondary block to appear within twitter rows

---
+++ sites/all/themes/Porto_sub/theme/views/views-view-unformatted--twitter-messages--tweets-block.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+ 
+  $counter = 0;
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <?php 
+    if(($secondary_ad_block == '') && ($counter == 3)){
+      break 1;
+    } 
+  ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+  <?php ++$counter; ?>  
+  <?php if($counter == 2) :?>
+    <?php print "<div class='pull-right'>" . $secondary_ad_block . "</div>"; ?>
+  <?php endif; ?>
+<?php endforeach; ?>
+
+
